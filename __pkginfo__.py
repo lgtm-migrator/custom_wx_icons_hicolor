c21d4e27e297871ebec47e5c379930e04c271361
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-01 21:36:39 +0000

    Updated files with 'repo_helper'.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -44,9 +44,9 @@ modname = "wx_icons_hicolor"
 pypi_name = "wx_icons_hicolor"
 import_name = "wx_icons_hicolor"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-short_desc = "Hicolor icon theme for wxPython"
-__author__ = author = "Dominic Davis-Foster"
-author_email = "dominic@davis-foster.co.uk"
+short_desc = 'Hicolor icon theme for wxPython'
+__author__ = author = 'Dominic Davis-Foster'
+author_email = 'dominic@davis-foster.co.uk'
 github_username = "domdfcoding"
 web = github_url = "https://github.com/domdfcoding/custom_wx_icons_hicolor"
 repo_root = pathlib.Path(__file__).parent
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from __pkginfo__ import *  # pylint: disable=wildcard-import
 
 
 setup(
-		description="Hicolor icon theme for wxPython",
+		description='Hicolor icon theme for wxPython',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
